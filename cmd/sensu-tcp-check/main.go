c481e51c7f7c94f14e51b9579ca454630d916dd3
danielb <git@masterunit.net>
2020-08-05 17:59:16 +0200

    output metrics in influxdb line protocol

--- cmd/sensu-tcp-check/main.go
+++ cmd/sensu-tcp-check/main.go
@@ -11,18 +11,21 @@ import (
 func main() {
 	log.SetOutput(os.Stderr)
 
-	if len(os.Args) == 1 {
-		println("usage: sensu-tcp-check <host>:<port>")
+	if len(os.Args) != 3 {
+		println("usage: sensu-tcp-check <metricprefix> <host:port>")
 		os.Exit(2)
 	}
 
 	timeBegin := time.Now()
 
-	conn, err := net.DialTimeout("tcp", os.Args[1], 10*time.Second)
+	conn, err := net.DialTimeout("tcp", os.Args[2], 10*time.Second)
 	if err != nil {
 		log.Printf("ERROR: %s\n\n", err.Error())
-		fmt.Printf("%s %d %d\n", "sensu.tcp.available", 0, timeBegin.Unix())
-		fmt.Printf("%s %d %d\n", "sensu.tcp.duration", 0, timeBegin.Unix())
+		fmt.Printf("%s,check=tcp %s=%d,%s=%d %d\n",
+			os.Args[1],
+			"available", 0,
+			"duration", 0,
+			timeBegin.Unix())
 
 		if conn != nil {
 			conn.Close()
@@ -34,8 +37,11 @@ func main() {
 	responseTime := time.Since(timeBegin).Milliseconds()
 
 	log.Printf("OK\n\n")
-	fmt.Printf("%s %d %d\n", "sensu.tcp.available", 1, timeBegin.Unix())
-	fmt.Printf("%s %d %d\n", "sensu.tcp.duration", responseTime, timeBegin.Unix())
+	fmt.Printf("%s,check=tcp %s=%d,%s=%d %d\n",
+		os.Args[1],
+		"available", 1,
+		"duration", responseTime,
+		timeBegin.Unix())
 
 	_ = conn.Close()
 
